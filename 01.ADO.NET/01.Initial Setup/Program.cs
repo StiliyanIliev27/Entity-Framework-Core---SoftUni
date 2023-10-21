07199c4db11ed709d2a50d176998f6363d9d5ac8
Stiliyan_Iliev <142061336+StiliyanIliev27@users.noreply.github.com>
2023-10-22 02:46:44 +0300

    Add files via upload

---
+++ 01.ADO.NET/01.Initial Setup/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace _01.Initial_Setup
+{
+    internal class Program
+    {
+        private const string ConnectionString = "Server = DESKTOP-ME8RHIA;Integrated Security=true; TrustServerCertificate = True; MultipleActiveResultSets=true;";
+        static void Main(string[] args)
+        {
+            using SqlConnection sqlConnection = new SqlConnection(ConnectionString);
+            sqlConnection.Open();
+
+            CreateDB(sqlConnection);
+            CreateDBTables(sqlConnection);
+            InsertIntoDBTables(sqlConnection);
+        }
+
+        static void CreateDB(SqlConnection sqlConnection)
+        {
+            using SqlCommand cmd = new SqlCommand(SQLQueries.CreateDB, sqlConnection);
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Succesfully created MinionsDB!");
+        }
+
+        static void CreateDBTables(SqlConnection sqlConnection)
+        {
+            using SqlCommand cmd = new SqlCommand(SQLQueries.CreateDBTables, sqlConnection);
+            cmd.ExecuteNonQuery();
+            Console.WriteLine($"Created DB tables!");
+        }
+
+        static void InsertIntoDBTables(SqlConnection sqlConnection)
+        {
+            using SqlCommand cmd = new SqlCommand(SQLQueries.InsertIntoDBTables, sqlConnection);
+            Console.WriteLine($"Insertion into tables was succesfully! Rows affected: {cmd.ExecuteNonQuery()}");
+        }
+    }
+}
