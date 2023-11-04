c73d6704c147d497e85960d009f380cbead9d972
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2023-11-04 13:41:18 +0200

    Added some files

---
+++ 05.LINQ/05.LINQ-Exercises-MusicHub-6.0/MusicHub/Data/Models/Song.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MusicHub.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicHub.Data.Models
+{
+    public class Song
+    {
+        public Song()
+        {
+            Performers = new HashSet<Performer>();
+        }
+        [Key]
+        public int Id { get; set; }
+      
+        [MaxLength(ValidationConstants.SongNameMaxLength)]
+        public string Name { get; set; } = null!;
+        public TimeSpan Duration { get; set; } //TimeSpan is required by default.
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public SongGenres Genre { get; set; }
+        public int? AlbumId { get; set; }        
+        [ForeignKey(nameof(AlbumId))]
+        public Album? Album { get; set; }
+        public int? WriterId { get; set; }
+        [ForeignKey(nameof(WriterId))]
+        public Writer? Writer { get; set; }  
+        public decimal Price { get; set; }
+        public ICollection<Performer> Performers { get; set; }
+    }
+}
